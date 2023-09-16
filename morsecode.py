defb0c99d21cd077877b0a742c4b2790856bf869
Hafiz <144364412+duckslayer101@users.noreply.github.com>
2023-09-16 13:32:18 +0600

    added if/else statement

--- morsecode.py
+++ morsecode.py
@@ -1,32 +1,35 @@
 '''Morse Code Generator'''
 
 '''
-Morse code is a method used in telecommunication to encode text characters as standardized 
-sequences of two different signal durations, called dots and dashes, or dits and dahs. 
+Morse code is a method used in telecommunication to encode text characters as standardized
+sequences of two different signal durations, called dots and dashes, or dits and dahs.
 Morse code is named after Samuel Morse, one of the inventors of the telegraph.
 '''
 
 '''
-In the 'word_bag' variable, there are the morse codes. Only the space(' ') isn't the morse code here. 
-For space( ) we get ' / ' , this is necessary for understanding the coded messages as it separates encoded words 
+In the 'word_bag' variable, there are the morse codes. Only the space(' ') isn't the morse code here.
+For space( ) we get ' / ' , this is necessary for understanding the coded messages as it separates encoded words
 so that the user can understand their coded messege.
 '''
 
 WORD_BAG = {
-    'a':'.-', 'b':'-...', 'c':'-.-.', 'd':'-..', 'e':'.', 'f':'..-.', 'g':'--.', 
-    'h':'....', 'i':'..', 'j':'.---', 'k':'-.-', 'l':'.-..', 'm':'--', 'n':'-.', 
-    'o':'---', 'p':'.--.', 'q':'--.-', 'r':'.-.', 's':'...', 't':'-', 'u':'..-', 
-    'v':'...-', 'w':'.--', 'x':'-..-', 'y':'-.--', 'z':'--..',  '1':'.----', 
+    'a':'.-', 'b':'-...', 'c':'-.-.', 'd':'-..', 'e':'.', 'f':'..-.', 'g':'--.',
+    'h':'....', 'i':'..', 'j':'.---', 'k':'-.-', 'l':'.-..', 'm':'--', 'n':'-.',
+    'o':'---', 'p':'.--.', 'q':'--.-', 'r':'.-.', 's':'...', 't':'-', 'u':'..-',
+    'v':'...-', 'w':'.--', 'x':'-..-', 'y':'-.--', 'z':'--..',  '1':'.----',
     '2':'..---', '3':'...--', '4':'....-', '5':'.....', '6':'-....', '7':'--...'
-    , '8':'---..', '9':'----.', '0':'-----', '?':'..--..', '!':'-.-.--', '.':'.-.-.-', 
-    ',':'--..--', ';':'-.-.-.', ':':'---...', '+':'.-.-.','-':'-....-', '/':'-..-.', 
+    , '8':'---..', '9':'----.', '0':'-----', '?':'..--..', '!':'-.-.--', '.':'.-.-.-',
+    ',':'--..--', ';':'-.-.-.', ':':'---...', '+':'.-.-.','-':'-....-', '/':'-..-.',
     '=':'-...-', "'":'.----.', '"':'.-..-.', ')':'-.--.-', '(':'-.--.', ' ':' / '
     }
 
 def generator(a):
   s=''
   for i in a:
-    s+=WORD_BAG[i]+' '  #The extra space(' ') is for the space between letters in a word
+    if i in WORD_BAG.keys():
+      s+=WORD_BAG[i]+' '     #The extra space(' ') is for the space between letters in a word
+    else:
+      pass  
   return s
 
 b=input("Please give your statements: ").lower()
